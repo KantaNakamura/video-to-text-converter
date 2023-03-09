1798632078c05fbe1741e4462d59a0ab8471b721
KantaNakamura <0706363@gmail.com>
2023-03-09 14:01:01 +0800

    create video to audio func

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ import moviepy.editor as mp
 import speech_recognition as sr
 
 
+# clipにvideo()を代入
+clip = mp.VideoFileClip('video_recording.mov') 
+# clipからaudioを抜き出し、converted_audio.wavとして保存する
+clip.audio.write_audiofile('converted_audio.wav')  
